d07d3dd5282358859dd821ea76680bcc3245c1f5
Rogertheone <ln451920@dal.ca>
2025-10-18 15:50:44 -0400

    环境变量的配置

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 
-import App from './App.vue'
+import App from '@/App.vue'
 //获取应用实例对象
 const app = createApp(App)
 //安装element-plus
